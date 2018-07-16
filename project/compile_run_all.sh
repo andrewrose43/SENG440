665d6e9c83697d3bf29406c0b384e05af196e383
Andrew Rose <andrew.rose43@gmail.com>
2018-07-15 20:01:53 -0700

    Some more batch file progress

--- project/compile_run_all.sh
+++ project/compile_run_all.sh
@@ -3,3 +3,4 @@ gcc -std=c99 ./testbench_split/testbench.c -o ./testbench_split/testbench.exe -l
 echo "Input x value: $1"
 echo "Input y value: $2"
 ./testbench_split/testbench.exe $1 $2 > output.txt
+echo "Note: the testbench's \"[xyz]_i\" values become the CORDIC main's \"[xyz]_i_init\" values." >> output.txt
