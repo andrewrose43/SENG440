c16f9201c99103dd9f778b2974aa31261a29147c
Andrew Rose <andrew.rose43@gmail.com>
2018-07-22 14:50:45 -0700

    Added preemptive loading

--- project/cordic_V_fixed_point.c
+++ project/cordic_V_fixed_point.c
@@ -1,7 +1,7 @@
 //Derived from Mihai Sima's "Lesson 101: CORDIC" slideshow
 //Copyright 2017 Mihai Sima
 
-int z_table[15];
+int z_table[16]; //array grows to include dummy value and accommodate preemptive loading
 
 void cordic_V_fixed_point(int *x, int *y, int *z){
 
@@ -16,8 +16,8 @@ void cordic_V_fixed_point(int *x, int *y, int *z){
 	z_tmp = 0;
 
 	//THE HEART OF THE CODE
+	z_table_tmp  = z_table[0]; //Loading ahead of first iteration of loop
 	for (i=0; i<15; i++){ //we want 15 iterations
-		z_table_tmp = z_table[i]; //Load the value from memory well ahead of when it is needed
 		if (y_tmp > 0){
 			x_tmp_2 = x_tmp_1 + (y_tmp >> i);
 			y_tmp = y_tmp - (x_tmp_1 >> i);
@@ -28,6 +28,7 @@ void cordic_V_fixed_point(int *x, int *y, int *z){
 			y_tmp = y_tmp + (x_tmp_1 >> i);
 			z_tmp -= z_table_tmp;
 		}
+		z_table_tmp = z_table[i]; //Load the value from memory ahead of when it is needed
 		x_tmp_1 = x_tmp_2;
 	}
 	*x = x_tmp_1;
@@ -40,7 +41,7 @@ void cordic_V_fixed_point(int *x, int *y, int *z){
  * DONE - Have we enough registers to store ALL the temporary variables?
  * DONE - Get rid of y_tmp_2? It seems to have no real purpose. Could just use y_tmp_1 and save an assignment
  * DONE - Would it be possible to replace the if instruction with a predicate? -O3???
- * -The z table obviously requires memory access; perhaps do this in advance of the if
+ * DONE -The z table obviously requires memory access; perhaps do this in advance of the if
  *  	-And/or in advance of the i<15 loop
  * -Load values for next iteration of loop while still in previous iteration
  * -Replace the current z_tmp load with z_table[i+1]
--- project/cordic_fixed_point.c
+++ project/cordic_fixed_point.c
@@ -5,7 +5,7 @@
 //Derived from Mihai Sima's "Lesson 101: CORDIC" slideshow
 //Copyright 2017 Mihai Sima
 
-int z_table[15] = {25735, 15192, 8027, 4074, 2045, 1023, 511, 255, 127, 63, 31, 15, 7, 3, 1};
+int z_table[16] = {25735, 15192, 8027, 4074, 2045, 1023, 511, 255, 127, 63, 31, 15, 7, 3, 1, 0};
 
 
 void cordic_V_fixed_point(int *x, int *y, int *z); //defined elsewhere
