66d94c52d3ef204b6d2cec951542b79820466f49
andrewrose43 <32423642+andrewrose43@users.noreply.github.com>
2018-07-29 22:21:00 -0700

    Update HW_cordic_V_fixed_point.c

--- project/HW_cordic_V_fixed_point.c
+++ project/HW_cordic_V_fixed_point.c
@@ -6,6 +6,7 @@ void HW_cordic_V_fixed_point(int *x, int *y, int *z){
 	y_tmp = *y;
 
 	//Custom instruction which calls the firmware or hardware computing unit
+	//Outputs stored in xz_tmp because the language only permits one output
 	__asm__ __volatile__ ("CORDIC_V %0, %1, %2"
 		: "=r" (xz_tmp) : "r" (x_tmp), "r" (y_tmp));
 
